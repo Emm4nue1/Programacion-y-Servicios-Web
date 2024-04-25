e645bde0c2892545be3c9713c2383a81af185d8a
Emm4nue1 <ema.mendoza97@outlook.com>
2024-04-25 00:53:54 -0300

    Actualizacion

--- 2023/Tp3/src/app/componentes/punto1/punto1.component.ts
+++ 2023/Tp3/src/app/componentes/punto1/punto1.component.ts
@@ -12,7 +12,10 @@ export class Punto1Component implements OnInit {
   constructor() { 
     this.productos =[
       { nombre: "notebook asus 13L", descripcion: "disco 40GB, 15pulgadas", img: "notebook13l.jpg", precio: 45.5},
-      { nombre: "Monitor LG 14", descripcion: "texto descriptivo producto 02", img: "producto02.jpg", precio: 99}
+      { nombre: "Monitor LG 14", descripcion: "texto descriptivo producto 02", img: "producto02.jpg", precio: 99},
+      { nombre: "Monitor LG 14", descripcion: "texto descriptivo producto 02", img: "producto02.jpg", precio: 99},
+      { nombre: "Monitor LG 14", descripcion: "texto descriptivo producto 02", img: "producto02.jpg", precio: 99},
+      { nombre: "Monitor LG 14", descripcion: "texto descriptivo producto 02", img: "producto02.jpg", precio: 99},
       { nombre: "Monitor LG 14", descripcion: "texto descriptivo producto 02", img: "producto02.jpg", precio: 99}
     ]
   }
