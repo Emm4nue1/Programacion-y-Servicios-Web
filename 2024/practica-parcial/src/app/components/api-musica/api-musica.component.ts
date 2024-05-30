adb018651cbb787dd07f9fd580e76a15d6a0d7bb
Emanuels97 <ema.mendo97@gmail.com>
2024-05-29 21:36:41 -0300

    actualizar

--- 2024/practica-parcial/src/app/components/api-musica/api-musica.component.ts
+++ 2024/practica-parcial/src/app/components/api-musica/api-musica.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MusicaService } from './service/musica.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-api-musica',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './api-musica.component.html',
   styleUrl: './api-musica.component.css'
 })
 export class ApiMusicaComponent {
+  cocina: any [] = [];
+  rta?:any;
+  constructor(private recta:MusicaService){
+    this.obtenerReceta();
+  }
+
+  obtenerReceta(){
+    this.recta.getReceta().subscribe((data: any) => {
+      this.cocina = data;
+    })
+  }
+
+  mostrarDetalles(txt:string){
+    this.recta.getDetalle(txt).subscribe((data1: any) => {
+      this.rta=data1.Ingredientes;
+      console.log(this.rta);
+    })
+  }
 
 }
--- 2024/practica-parcial/src/app/components/api-musica/service/musica.service.ts
+++ 2024/practica-parcial/src/app/components/api-musica/service/musica.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicaService {
+private url="https://recipe-book2.p.rapidapi.com/recipes-popular";
+  constructor(private htpp: HttpClient) { }
 
-  constructor() { }
+  getReceta():Observable<any>{
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': 'e1dce37a05msh1434ca70e470895p1255fajsn19a76c057f19',
+      'x-rapidapi-host': 'recipe-book2.p.rapidapi.com'
+    });
+    return this.htpp.get(this.url, { headers: headers });
+  }
+
+
+  getDetalle(txt:string):Observable<any>{
+    const url1="https://recipe-book2.p.rapidapi.com/recipe-details?path="+txt;
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': 'e1dce37a05msh1434ca70e470895p1255fajsn19a76c057f19',
+      'x-rapidapi-host': 'recipe-book2.p.rapidapi.com'
+    });
+
+    return this.htpp.get(url1, { headers: headers});
+  }
+
+
 }
