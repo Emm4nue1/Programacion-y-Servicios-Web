b170f97d3996e616782a33902609afc8f6e15b1c
Emanuels97 <ema.mendo97@gmail.com>
2024-06-01 19:31:17 -0300

    actualizacion

--- 2024/practica-parcial/src/app/components/api-clima/api-clima.component.ts
+++ 2024/practica-parcial/src/app/components/api-clima/api-clima.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ClimaService } from './service/clima.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-api-clima',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './api-clima.component.html',
   styleUrl: './api-clima.component.css'
 })
 export class ApiClimaComponent {
+  z?:any;
+  detalles?:any;
+  constructor(private autor:ClimaService) {
+    this.obtenerReceta();
+  }
 
+  obtenerReceta(){
+    this.autor.getAutor().subscribe((data: any) => {
+      this.z = data;
+    })
+  }
+
+  ObteneDetalle(id:number){
+    this.autor.getDetalle(id).subscribe((data1:any)=>{
+      this.detalles=data1;
+    })
+  }
 }
--- 2024/practica-parcial/src/app/components/api-clima/service/clima.service.ts
+++ 2024/practica-parcial/src/app/components/api-clima/service/clima.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
+private x="https://hapi-books.p.rapidapi.com/top_authors";
 
-  constructor() { }
+
+
+  constructor(private htpp: HttpClient) { }
+
+  getAutor():Observable<any>{
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': 'fc4632e173msh84c3f3b62b5615bp13bc20jsn43ab32a5a833',
+      'x-rapidapi-host': 'hapi-books.p.rapidapi.comhapi-books.p.rapidapi.com'
+    });
+    return this.htpp.get(this.x, { headers: headers });
+  }
+
+  getDetalle(id:number):Observable<any>{
+    const url1="https://hapi-books.p.rapidapi.com/author/"+id;
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': 'fc4632e173msh84c3f3b62b5615bp13bc20jsn43ab32a5a833',
+      'x-rapidapi-host': 'hapi-books.p.rapidapi.com'
+    });
+
+    return this.htpp.get(url1, { headers: headers});
+  }
+
 }
